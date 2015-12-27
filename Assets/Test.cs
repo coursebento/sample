886dae3991d52b284aad0d53a4479684165135f7
CourseBento <course@progaming.co.th>
2015-12-27 17:01:01 +0700

    Make it perfect

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -6,8 +6,10 @@ public class Test : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		Debug.Log ("A");
-		Debug.Log ("Y");
+		Debug.Log ("B");
 		Debug.Log ("C");
+		Debug.Log ("D");
+		Debug.Log ("E");
 	}
 	
 	// Update is called once per frame
